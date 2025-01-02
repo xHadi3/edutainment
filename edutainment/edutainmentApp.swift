8627c42e2baa883ee7590961f864326a2b1b6c16
xHadi3 <hadiessam9@gmail.com>
2025-01-02 17:40:19 +0300

    Initial Commit

---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Hadi Al zayer on 02/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
